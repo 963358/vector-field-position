8fc4fbd83bc1b6d01dea1dca284087cb170a7b35
ryan <ryan963358@gmail.com>
2024-05-16 13:39:43 -0700

    progress

--- src/field.py
+++ src/field.py
@@ -12,8 +12,8 @@ Demonstrates plotting directional arrows at points on a 3D meshgrid.
 import matplotlib.pyplot as plt
 import numpy as np
 
-def init_point():
-    init_x, init_y, init_z = 1,1,1
+def init_point(x,y,z):
+    ax.scatter(x,y,z, c="red", s=100)
 
 def create_vectors():
     
@@ -23,32 +23,43 @@ def create_vectors():
                 )
 
     # modify the boundaries
-    # u cross v
+    # u cross w, direction of v axis 
+    # back
     u[0,:] = 0
-    v[0,:] = 0
-    w[0,:] = 1
+    v[0,:] = 1
+    w[0,:] = 0
 
+    # front
     u[-1,:] = 0
-    v[-1,:] = 0
-    w[-1,:] = -1
+    v[-1,:] = -1
+    w[-1,:] = 0
 
-    # v cross w
-    u[:,:,-1] = 1
+    # u cross v, direction of w 
+    # top 
+    u[:,:,-1] = 0
     v[:,:,-1] = 0
-    w[:,:,-1] = 0
+    w[:,:,-1] = -1
 
-    u[:,:,0] = -1
+    # corners
+
+    # bottom
+    u[:,:,0] = 0
     v[:,:,0] = 0
-    w[:,:,0] = 0
+    w[:,:,0] = 1
 
-    # u cross w
-    u[:,0] = 0
-    v[:,0] = 1
+
+    # v cross w, direction of u
+    # left 
+    u[:,0] = 1
+    v[:,0] = 0
     w[:,0] = 0
 
-    u[:,-1] = 0
-    v[:,-1] = -1
+    # right
+    u[:,-1] = -1
+    v[:,-1] = 0
     w[:,-1] = 0
+
+    
 
     return u, v, w
 
@@ -66,7 +77,10 @@ x, y, z = np.meshgrid(np.arange(-3, 4, 1),
 u, v, w = create_vectors()
 
 
+# x, y, z
+init_point(1,2,3)
 
-ax.quiver(x, y, z, u, v, w, length=1, normalize=True)
+
+ax.quiver(x, y, z, u, v, w, length=0.5, normalize=True)
 
 plt.show()
