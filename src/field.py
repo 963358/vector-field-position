c2ed6d4719a5c66888f0123b842c0c9161577087
ryan <ryan963358@gmail.com>
2024-05-17 09:14:55 -0700

    GervE

--- src/field.py
+++ src/field.py
@@ -1,11 +1,3 @@
-"""
-==============
-3D quiver plot
-==============
-
-Demonstrates plotting directional arrows at points on a 3D meshgrid.
-"""
-
 # max possible velocity is max magnitude of vector (1) times grid size (7)
 # make sure to model time interval so that it does not skip positions 
 
@@ -15,6 +7,9 @@ import numpy as np
 
 def init_point(x,y,z):
     ax.scatter(x,y,z, c="red", s=100)
+
+def init_velocity(u,v,w):
+    
 
 def create_vectors():
     
@@ -27,46 +22,45 @@ def create_vectors():
     # u cross w, direction of v axis 
     # back
     u[0,:] = 0
-    v[0,:] = 1
     w[0,:] = 0
-
     # front
     u[-1,:] = 0
-    v[-1,:] = -1
     w[-1,:] = 0
 
     # u cross v, direction of w 
     # top 
     u[:,:,-1] = 0
     v[:,:,-1] = 0
-    w[:,:,-1] = -1
-
-    # corners
-
     # bottom
     u[:,:,0] = 0
     v[:,:,0] = 0
-    w[:,:,0] = 1
-
 
     # v cross w, direction of u
     # left 
-    u[:,0] = 1
     v[:,0] = 0
     w[:,0] = 0
-
     # right
-    u[:,-1] = -1
     v[:,-1] = 0
     w[:,-1] = 0
 
-    n=6
+    # direction of v
+    # back
+    v[0,:] = 1
+    # front
+    v[-1,:] = -1
+    
+    # direction of w
+    # top
+    w[:,:,-1] = -1
+    # bottom
+    w[:,:,0] = 1    
+    
+    # direction of u
+    # left 
+    u[:,0] = 1 
+    # right
+    u[:,-1] = -1
 
-    u[n-n,n-n,n-n] = v[n-n,n-n,n-n] = w[n-n,n-n,n-n] = v[n,n-n,n-n] = w[n,n-n,n-n] = u[n-n,n-n,n] = v[n-n,n-n,n] = u[n-n,n,n] = u[n,n-n,n] = v[n,n-n,n] = w[n,n,n-n] = u[n-n,n,n-n] = w[n-n,n,n-n] = 1
-
-    u[n,n,n] = v[n,n,n] = w[n,n,n] = u[n,n-n,n-n] = w[n-n,n-n,n] = v[n-n,n,n] = w[n-n,n,n] = w[n,n-n,n] = u[n,n,n-n] = v[n,n,n-n] = v[n-n,n,n-n] = -1
-
-    print(u[-3,-3,-3], v[-3,-3,-3], w[-3,-3,-3])
 
     return u, v, w
 
@@ -79,12 +73,11 @@ x, y, z = np.meshgrid(np.arange(-3, 4, 1),
                      )
                      
 
-
 u, v, w = create_vectors()
 
 
 # x, y, z
-init_point(1,2,3)
+init_point(np.random.uniform(-3,3), np.random.uniform(-3,3), np.random.uniform(-3,3))
 
 
 ax.quiver(x, y, z, u, v, w, length=0.5, normalize=True)
