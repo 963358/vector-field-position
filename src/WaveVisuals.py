ce3f276d94911eef870ee31652e5b52bfa9b8f04
ryan <ryan963358@gmail.com>
2024-05-19 17:24:38 -0700

    rveve

--- src/WaveVisuals.py
+++ src/WaveVisuals.py
@@ -1,37 +1,25 @@
 import matplotlib.pyplot as plt
 from matplotlib import animation
-from matplotlib import cm
-from mpl_toolkits import mplot3d
 import numpy as np
-import math as m
 
 def Set_hydrophones():
-    plt.plot(1, 0, 0, 'ro')
+    plt.plot(10, 0, 0, 'ro')
     plt.plot(0, 0, 0, 'ro')
-    plt.plot(0, 1, 0, 'ro')
-    plt.plot(0, 0, 1, 'ro')
+    plt.plot(0, 10, 0, 'ro')
+    plt.plot(0, 0, 10, 'ro')
 
 fig = plt.figure()
-plt.xlabel('x')
-plt.ylabel('y')
+ax = fig.add_subplot(projection ='3d')
 
-ax = plt.axes(projection ='3d')
-ax.set_xlim(0, 20)
-ax.set_ylim(0, 20)
-ax.set_zlim(0, 20)
 
-Set_hydrophones()
-
-plt.plot(20, 20, 20, 'ro')
 
 u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
 r = 0.1
 x = r*np.cos(u)*np.sin(v) + 20
 y = r*np.sin(u)*np.sin(v) + 20
 z = r*np.cos(v) + 20
-ax.plot_wireframe(x, y, z, color="g")
 
-d = m.sqrt(m.pow(20, 2)*3) #distance of hydrophones to the 
+d = np.sqrt(np.power(20, 2)*3) #distance of hydrophones to the 
 
 
 def init():
@@ -41,7 +29,8 @@ def init():
 def animate(i):
     # remove previous collections
     ax.clear()
-
+    
+    Set_hydrophones()
     # add the new sphere
     x = (r+0.3*i)*np.cos(u)*np.sin(v) + 20
     y = (r+0.3*i)*np.sin(u)*np.sin(v) + 20
@@ -54,6 +43,10 @@ def animate(i):
     ax.set_ylim(0, 50)
     ax.set_zlim(0, 50)
 
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+
     if((r+0.3*i) > d):
         ani.event_source.stop()
     
@@ -62,23 +55,5 @@ def animate(i):
     return fig,
 
 ani = animation.FuncAnimation(fig, animate, init_func = init, frames = 90, interval = 100)
+
 plt.show()
-
-# def run_animation():
-#     anim_running = True
-
-#     def onClick(event):
-#         nonlocal anim_running
-#         if anim_running:
-#             anim.event_source.stop()
-#             anim_running = False
-#         else:
-#             anim.event_source.start()
-#             anim_running = True
-
-#     def animFunc( ...args... ):
-#         # Animation update function here
-
-#     fig.canvas.mpl_connect('button_press_event', onClick)
-
-#     anim = animation.FuncAnimation(fig, animFunc[,...other args])
